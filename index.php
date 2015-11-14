915761b4e6c7a26209ca6c0166fae14ffc62894c
John Reese <jpreese@mtu.edu>
2015-11-14 11:44:36 -0500

    Wire up post data and initial error reporting

--- index.php
+++ index.php
@@ -1,4 +1,69 @@
-﻿<!DOCTYPE html>
+﻿<?php
+
+// setup server and client error reporting
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+function print_error_if_exists($error)
+{
+	if($error)
+	{
+		echo "<i>Invalid format.</i>";
+	}
+}
+
+// establish database connection
+$conn = mysql_connect("localhost", "root", "");
+mysql_select_db("main", $conn);
+
+if(isset($_POST['submit']))
+{
+	// initialize and escape post data
+	$umid = mysql_real_escape_string($_POST['inputUMID']);
+	$firstName = mysql_real_escape_string($_POST['inputFirstName']);
+	$lastName = mysql_real_escape_string($_POST['inputLastName']);
+	$projectTitle = mysql_real_escape_string($_POST['inputProjectTitle']);
+	$email = mysql_real_escape_string($_POST['inputEmail']);
+	$phone = mysql_real_escape_string($_POST['inputPhone']);
+	$timeslot = mysql_real_escape_string($_POST['inputTimeSlot']);
+
+	// validate umid
+	$pattern = "/^[0-9]{8}$/";
+	if(preg_match($pattern, $umid) == false)
+	{
+		$umidError = true;
+	}
+	
+	// validate first name
+	$pattern = "/^[0-9]{8}$/";
+	if(preg_match($pattern, $firstName) == false)
+	{
+		$firstNameError = true;
+	}
+	
+	// validate last name
+	$pattern = "/^[0-9]{8}$/";
+	if(preg_match($pattern, $lastName) == false)
+	{
+		$lastNameError = true;
+	}
+	
+	// validate email
+	$pattern = "/^[0-9]{8}$/";
+	if(preg_match($pattern, $email) == false)
+	{
+		$emailError = true;
+	}
+	
+	// validate phone number
+	$pattern = "/^[0-9]{8}$/";
+	if(preg_match($pattern, $phone) == false)
+	{
+		$phoneError = true;
+	}
+}
+
+?>
+
+<!DOCTYPE html>
 
 <html>
 <head>
@@ -38,69 +103,67 @@
 
             <!-- registration form -->
             <div class="col-lg-7">
-                <form class="form-horizontal">
+                <form class="form-horizontal" method="post" action="index.php">
                     <fieldset>
                         <div class="form-group">
                             <label for="inputUMID" class="col-lg-2 control-label">UMID</label>
                             <div class="col-lg-10">
-                                <input type="text" class="form-control" id="inputUMID" maxlength="8" pattern="[0-9]{8}" required>
+                                <input type="text" class="form-control" name="inputUMID" maxlength="8"><!-- pattern="[0-9]{8}" required> --> <?php print_error_if_exists($umidError) ?>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputFirstName" class="col-lg-2 control-label">First Name</label>
                             <div class="col-lg-10">
-                                <input type="text" class="form-control" id="inputFirstName" pattern="[a-zA-Z]" required>
+                                <input type="text" class="form-control" name="inputFirstName"><!-- pattern="[a-zA-Z]" required> --> <?php print_error_if_exists($firstNameError) ?>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputLastName" class="col-lg-2 control-label">Last Name</label>
                             <div class="col-lg-10">
-                                <input type="text" class="form-control" id="inputLastName" pattern="[a-zA-Z]" required>
+                                <input type="text" class="form-control" name="inputLastName"><!-- pattern="[a-zA-Z]" required> --> <?php print_error_if_exists($lastNameError) ?>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputProjectTitle" class="col-lg-2 control-label">Project Title</label>
                             <div class="col-lg-10">
-                                <input type="text" class="form-control" id="inputProjectTitle" required>
+                                <input type="text" class="form-control" name="inputProjectTitle"><!-- required> -->
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputEmail" class="col-lg-2 control-label">Email</label>
                             <div class="col-lg-10">
-                                <input type="text" class="form-control" id="inputEmail" pattern="[a-z0-9]+@([a-z0-9]+)(.[a-z0-9])+." required>
+                                <input type="text" class="form-control" name="inputEmail"><!-- pattern="[a-z0-9]+@([a-z0-9]+)(.[a-z0-9])+." required> --> <?php print_error_if_exists($emailError) ?>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputPhone" class="col-lg-2 control-label">Phone</label>
                             <div class="col-lg-10">
-                                <input type="text" class="form-control" id="inputPhone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+                                <input type="text" class="form-control" name="inputPhone"><!-- pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required> --> <?php print_error_if_exists($phoneError) ?>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="select" class="col-lg-2 control-label">Time Slot</label>
                             <div class="col-lg-10">
-                                <select class="form-control" id="select">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
+                                <select class="form-control" name="inputTimeSlot">
+								<?php
+									$query = mysql_query("select * from timeslots");
+									while($row = mysql_fetch_array($query))
+									{
+										echo "<option id=\"" . $row['timeslotid'] . "\">" . $row['start_time'] . " - " . $row['end_time'] . "</option>";
+									}
+								?>
                                 </select>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="col-lg-10 col-lg-offset-2">
                                 <button type="reset" class="btn btn-default">Cancel</button>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
                             </div>
                         </div>
                     </fieldset>
                 </form>
             </div>
-
-            <!-- availability -->
-
-
         </div>
     </div>
 
