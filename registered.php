619390a9f451cf70d7168227a5d1d9d8a4dad545
John Reese <jpreese@mtu.edu>
2015-11-20 18:33:29 -0500

    Render registered users grid

--- registered.php
+++ registered.php
@@ -2,6 +2,33 @@
 
 require_once("common.php");
 
+function render_registered_students_rows()
+{
+	$query = "SELECT * FROM students AS S";
+	$rslt = mysql_query($query);
+	
+	while($row = mysql_fetch_array($rslt))
+	{
+		echo "<tr>";
+		echo "<td>" . $row['umid'] . "</td>";
+		echo "<td>" . $row['firstname'] . "</td>";
+		echo "<td>" . $row['lastname'] . "</td>";
+		echo "<td>" . $row['email'] . "</td>";
+		echo "<td>" . $row['phone'] . "</td>";
+		echo "<td>" . $row['projectname'] . "</td>";
+		echo "<td>" . get_time_from_id($row['timeslotid']) . "</td>";
+		echo "</tr>";
+	}
+}
+
+function get_time_from_id($timeslotid)
+{
+	$query = "SELECT start_time, end_time FROM timeslots AS T WHERE T.timeslotid = '$timeslotid'";
+	$rslt = mysql_fetch_array(mysql_query($query));
+	
+	return $rslt['start_time'] . " - " . $rslt['end_time'];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -41,7 +68,22 @@ require_once("common.php");
 
     <div class="container">
         <div class="row">
-			stuff here		
+			<table class="table table-striped table-hover ">
+				<thead>
+					<tr>
+						<th>UMID</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th>Project Name</th>
+						<th>Time</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php render_registered_students_rows() ?>
+				</tbody>
+			</table>	
         </div>
     </div>
 
